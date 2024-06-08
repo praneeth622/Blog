d557278a8d5f9be7f6c4caab00cc525cf28ddc31
Devarasetty Praneeth <89689768+praneeth622@users.noreply.github.com>
2024-06-08 16:34:19 +0000

    created routes and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,94 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign,verify} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:String
+  }
+}>()
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('This is user sign up page')
+app.use('/api/v1/blog/*',async(c,next)=>{
+
+  //fetch the header
+  const header = c.req.header('autharization') || ""
+
+  //verify the jwt
+  //@ts-ignore
+  const response =await verify(header,c.env.JWT_SECRET)
+
+  if(!response.id){
+    c.status(403)
+    return c.json({
+      "message":"User unauthoiraize"
+    })
+  }else{
+    next()
+  }
+
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('This is user sign in page')
+app.post('/api/v1/user/signup', async(c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json()
+  console.log("User data is ", body)
+  
+  const user = await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password
+    }
+  })
+
+  //user Verification
+  if(!user){
+    return Response.json({
+      message :"Failed to sign up "
+    })
+  }
+
+  //jwt token
+  //@ts-ignore
+  const token = await sign({id:user.id},c.env.JWT_SECRET)
+  
+  return c.json({
+    "jwt":token
+  })
+})
+
+app.post('/api/v1/user/signin', async(c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+
+  const user = await prisma.user.findUnique({
+    where:{
+      email:body.email,
+      password:body.password
+    }
+  })
+
+  //User Not present 
+  if(!user){
+    return c.json({
+      "Message":"User Not Present"
+    })
+  }
+
+  //@ts-ignore
+  const token = await sign({id:user.id},c.env.JWT_SECRET)
+
+  return c.json({
+    "jwt":token
+  })
 })
 
 app.post('/api/v1/blog', (c) => {
